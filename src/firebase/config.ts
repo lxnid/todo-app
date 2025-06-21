6800820123364e328c806cf387301b7caba3f99b
lxnid <dinilr28@outlook.com>
2025-06-21 07:03:22 +0530

    fix(firebase): correct storage bucket url in config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAo_bfivWDMhsdboWup756fzSdyK4WIa-Y",
   authDomain: "to-do-bcb51.firebaseapp.com",
   projectId: "to-do-bcb51",
-  storageBucket: "to-do-bcb51.firebasestorage.app",
+  storageBucket: "to-do-bcb51.appspot.com",
   messagingSenderId: "1068599821608",
   appId: "1:1068599821608:web:94c79d4e4d4c0c5478bc1d",
   measurementId: "G-RSGB54X1DM"
